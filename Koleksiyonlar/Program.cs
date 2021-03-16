ad37eecf3b34a3ae1cc3d08f69a714ed0dd5041c
HIbrahimBircan@outlook.com <HIbrahimBircan@outlook.com>
2021-03-16 22:16:30 +0300

    Liste oluşturma ile alakalı

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> islemler2 =new List<string> {"Engin","Murat","Kerem","Halil" };
+            
+
+            Console.WriteLine(islemler2[0]);
+            Console.WriteLine(islemler2[1]);
+            Console.WriteLine(islemler2[2]);
+            Console.WriteLine(islemler2[3]);
+            islemler2.Add("İlker");
+            Console.WriteLine(islemler2[4]);
+            Console.WriteLine(islemler2[0]);
+        }
+    }
+}
